9a28736d00ea38e556dacc156f436fadde23ee04
Avinashkr000 <akum97543@gmail.com>
2025-01-19 19:13:07 +0530

    Added

---
+++ Level-01/Constructors/HotelBooking.java
@@ -0,0 +1,40 @@
+package Learn;
+
+
+
+class HotelBooking {
+
+    String guestName;
+    String roomType;
+    int nights;
+
+    public HotelBooking() {
+        this.guestName = "Default Guest";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    public void displayDetails() {
+        System.out.println("Guest Name: " + guestName);
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking booking = new HotelBooking();
+        booking.displayDetails();
+    }
+}
+
